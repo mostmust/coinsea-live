bb76bfb3bd3f15adc0ff251ef62653a4d9ca0e4b
mostmust <mostmust@naver.com>
2025-07-07 01:58:15 +0900

    Update exchangeRate.js

--- netlify/functions/exchangeRate.js
+++ netlify/functions/exchangeRate.js
@@ -6,7 +6,7 @@ exports.handler = async () => {
     const data = await res.json();
 
     if (!data || !data.rates || !data.rates.KRW) {
-      throw new Error('환율 정보 없음 (data.rates.KRW)');
+      throw new Error("환율 정보 없음 (data.rates.KRW)");
     }
 
     return {
@@ -14,15 +14,15 @@ exports.handler = async () => {
       body: JSON.stringify({
         usdToKrw: data.rates.KRW,
         date: data.time_last_update_utc
-      })
+      }),
     };
   } catch (error) {
     return {
       statusCode: 500,
       body: JSON.stringify({
-        error: '데이터 로딩 실패',
-        detail: error.message
-      })
+        error: "데이터 로딩 실패",
+        detail: error.message,
+      }),
     };
   }
 };
