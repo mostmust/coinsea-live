8af6b1b485c7849ae9f539e8768c4a5bb603c954
mostmust <mostmust@naver.com>
2025-07-06 23:44:10 +0900

    Update exchangeRate.js

--- netlify/functions/exchangeRate.js
+++ netlify/functions/exchangeRate.js
@@ -2,24 +2,31 @@ const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fet
 
 exports.handler = async () => {
   try {
-    const res = await fetch('https://api.exchangerate.host/latest?base=USD&symbols=KRW');
+    const res = await fetch('https://api.exchangerate.host/latest?base=USD&symbols=KRW', {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
     const fx = await res.json();
 
     if (!fx.rates || !fx.rates.KRW) {
-      throw new Error("환율 정보 없음 (fx.rates.KRW)");
+      throw new Error(`환율 정보 없음 (fx.rates.KRW)`);
     }
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ usdToKrw: fx.rates.KRW }),
+      body: JSON.stringify({
+        usdToKrw: fx.rates.KRW
+      })
     };
   } catch (error) {
     return {
       statusCode: 500,
       body: JSON.stringify({
-        error: "데이터 로딩 실패",
-        detail: error.message,
-      }),
+        error: '데이터 로딩 실패',
+        detail: error.message
+      })
     };
   }
 };
