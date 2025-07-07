8d21a07b16fa360690ccd4d9069b8ac027417148
mostmust <mostmust@naver.com>
2025-07-07 13:22:03 +0900

    Update exchangeRate.js

--- netlify/functions/exchangeRate.js
+++ netlify/functions/exchangeRate.js
@@ -3,14 +3,15 @@ const fetch = require("node-fetch");
 exports.handler = async function (event, context) {
   try {
     const API_KEY = "d900b09afec85ec2f5f506a607dbb958";
-    const url = `https://api.exchangerate.host/latest?base=USD&symbols=KRW&access_key=${API_KEY}`;
+    const url = `https://api.exchangerate.host/live?access_key=${API_KEY}&currencies=KRW&source=USD&format=1`;
+
     const response = await fetch(url);
     const data = await response.json();
 
-    if (data.success && data.rates && data.rates.KRW) {
+    if (data.success && data.quotes && data.quotes.USDKRW) {
       return {
         statusCode: 200,
-        body: JSON.stringify({ krw: data.rates.KRW }),
+        body: JSON.stringify({ krw: data.quotes.USDKRW }),
       };
     } else {
       return {
