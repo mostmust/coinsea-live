d51fe4801024f4955ebec30ed01aae974db280e7
mostmust <mostmust@naver.com>
2025-07-07 13:43:34 +0900

    Update exchangeRate.js

--- netlify/functions/exchangeRate.js
+++ netlify/functions/exchangeRate.js
@@ -3,24 +3,19 @@ const fetch = require("node-fetch");
 exports.handler = async function (event, context) {
   try {
     const API_KEY = "d900b09afec85ec2f5f506a607dbb958";
-    const url = `https://api.exchangerate.host/latest?base=USD&symbols=KRW&access_key=${API_KEY}`;
+    const url = `https://api.exchangerate.host/live?access_key=${API_KEY}&currencies=KRW&source=USD&format=1`;
+
     const response = await fetch(url);
     const data = await response.json();
 
-    if (data && data.rates && data.rates.KRW) {
+    if (data.success && data.quotes && data.quotes.USDKRW) {
       return {
         statusCode: 200,
-        headers: {
-          "Access-Control-Allow-Origin": "*", // ✅ CORS 해결
-        },
-        body: JSON.stringify({ krw: data.rates.KRW }),
+        body: JSON.stringify({ krw: data.quotes.USDKRW }),
       };
     } else {
       return {
         statusCode: 500,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
         body: JSON.stringify({
           error: "환율 정보 없음",
           message: "KRW 환율 데이터가 존재하지 않습니다.",
@@ -31,9 +26,6 @@ exports.handler = async function (event, context) {
   } catch (error) {
     return {
       statusCode: 500,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
       body: JSON.stringify({
         error: "서버 오류",
         message: error.message,
